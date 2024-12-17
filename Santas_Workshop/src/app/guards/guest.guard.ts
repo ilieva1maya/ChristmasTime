d8321e845e467a0a1a010b4a26e78df474362974
Maya Ilieva <ilieva1maya@gmail.com>
2024-12-17 16:45:05 +0200

    Guest guard implemented
    -login
    -register

---
+++ Santas_Workshop/src/app/guards/guest.guard.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { UserService } from '../user/user.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GuestGuard implements CanActivate {
+
+  constructor(private userService: UserService, private router: Router) {}
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+    
+    if (!this.userService.isLogged) {
+      return true;  
+    }
+
+    window.alert("You are logged in!")
+
+    this.router.navigate(['/warehouse']);
+    return false;
+  }
+}
--- Santas_Workshop/src/app/user/user-routing.module.ts
+++ Santas_Workshop/src/app/user/user-routing.module.ts
@@ -3,12 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { ProfileComponent } from './profile/profile.component';
+import { GuestGuard } from '../guards/guest.guard';
 
 
 const routes: Routes = [
-  { path: 'login', component: LoginComponent, },
-  { path: 'register', component: RegisterComponent, },
-  { path: 'profile', component: ProfileComponent, }
+  { path: 'login', component: LoginComponent, canActivate: [GuestGuard]},
+  { path: 'register', component: RegisterComponent, canActivate: [GuestGuard]},
+  { path: 'profile', component: ProfileComponent, canActivate: [GuestGuard]}
 ];
 
 @NgModule({
