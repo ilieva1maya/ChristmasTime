8b29124d0c5c480225a7a7ddb15cf059a2b93aa5
Maya Ilieva <ilieva1maya@gmail.com>
2024-12-18 14:00:26 +0200

    Getting the presntID in api.service

--- Santas_Workshop/src/app/api.service.ts
+++ Santas_Workshop/src/app/api.service.ts
@@ -2,7 +2,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment.development';
 import { Present } from './types/present';
-import { catchError, EMPTY, Observable } from 'rxjs';
+import { BehaviorSubject, catchError, EMPTY, Observable, Subscription } from 'rxjs';
 import { Router } from '@angular/router';
 import { Reservation } from './types/reservation';
 
@@ -10,15 +10,30 @@ import { Reservation } from './types/reservation';
   providedIn: 'root'
 })
 export class ApiService {
+  private present$$ = new BehaviorSubject<Present | undefined>(undefined);
+  private present$ = this.present$$.asObservable();
 
-  constructor(private http: HttpClient, private router: Router) { }
+  present: Present | undefined;
+  PRESENT_KEY = '[present]';
+
+  presentSubscription: Subscription;
+
+  // get isLogged(): boolean {
+  //   return !!this.user;
+  // }
+
+  constructor(private http: HttpClient, private router: Router) {
+    this.presentSubscription = this.present$.subscribe((present) => {
+      this.present = present;
+    });
+  }
 
   getPresents() {
     const { apiUrl } = environment;
     return this.http.get<Present[]>(`${apiUrl}/data/presents`);
   }
 
-  getReservations(){
+  getReservations() {
     const { apiUrl } = environment;
     return this.http.get<Reservation[]>(`${apiUrl}/data/reservations`);
   }
@@ -29,14 +44,14 @@ export class ApiService {
   }
 
   createPresent(itemName: string, itemDescription: string, itemImage: string, itemCategory: string, itemStatus: string, owner: string) {
-    const { apiUrl } = environment;   
+    const { apiUrl } = environment;
     return this.http.post<Present>(`${apiUrl}/data/presents`, { itemName, itemDescription, itemImage, itemCategory, itemStatus, owner });
   }
 
-  createReservation(reservationComment: string, userId: string, presentId:string){
+  createReservation(reservationComment: string, userId: string, presentId: string) {
     const { apiUrl } = environment;
     console.log(reservationComment, userId, presentId)
-    // return this.http.post<Reservation>(`${apiUrl}/data/reservations`, { reservationComment, userId, presentId, reservationId })
+    return this.http.post<Reservation>(`${apiUrl}/data/reservations`, { reservationComment, userId, presentId })
   }
 
   updatePresent(itemName: string, itemDescription: string, itemImage: string, itemCategory: string, itemStatus: string, id: string, owner: string): Observable<Present> {
@@ -72,3 +87,68 @@ export class ApiService {
     });
   }
 }
+
+// Working
+// export class ApiService {
+
+//   constructor(private http: HttpClient, private router: Router) { }
+
+//   getPresents() {
+//     const { apiUrl } = environment;
+//     return this.http.get<Present[]>(`${apiUrl}/data/presents`);
+//   }
+
+//   getReservations(){
+//     const { apiUrl } = environment;
+//     return this.http.get<Reservation[]>(`${apiUrl}/data/reservations`);
+//   }
+
+//   getPresentById(id: string) {
+//     const { apiUrl } = environment;
+//     return this.http.get<Present>(`${apiUrl}/data/presents/${id}`)
+//   }
+
+//   createPresent(itemName: string, itemDescription: string, itemImage: string, itemCategory: string, itemStatus: string, owner: string) {
+//     const { apiUrl } = environment;
+//     return this.http.post<Present>(`${apiUrl}/data/presents`, { itemName, itemDescription, itemImage, itemCategory, itemStatus, owner });
+//   }
+
+//   createReservation(reservationComment: string, userId: string, presentId:string){
+//     const { apiUrl } = environment;
+//     console.log(reservationComment, userId, presentId)
+//     // return this.http.post<Reservation>(`${apiUrl}/data/reservations`, { reservationComment, userId, presentId, reservationId })
+//   }
+
+//   updatePresent(itemName: string, itemDescription: string, itemImage: string, itemCategory: string, itemStatus: string, id: string, owner: string): Observable<Present> {
+//     const { apiUrl } = environment;
+//     const token = localStorage.getItem('accessToken');
+
+//     let headers = new HttpHeaders({
+//       'Content-Type': 'application/json'
+//     });
+
+//     if (token) {
+//       headers = headers.set('X-Authorization', token);
+//     }
+
+//     return this.http.put<Present>(`${apiUrl}/data/presents/${id}`,
+//       { itemName, itemDescription, itemImage, itemCategory, itemStatus, id, owner }, { headers });
+//   }
+
+//   deletePresent(id: string) {
+//     const { apiUrl } = environment;
+//     const token = localStorage.getItem('accessToken');
+
+//     let headers = new HttpHeaders({
+//       'Content-Type': 'application/json'
+//     });
+
+//     if (token) {
+//       headers = headers.set('X-Authorization', token);
+//     }
+
+//     return this.http.delete<Present>(`${apiUrl}/data/presents/${id}`).subscribe(() => {
+//       this.router.navigate(['/warehouse']);
+//     });
+//   }
+// }
--- Santas_Workshop/src/app/presents/reserve-present/reserve-present.component.ts
+++ Santas_Workshop/src/app/presents/reserve-present/reserve-present.component.ts
@@ -4,6 +4,8 @@ import { ApiService } from 'src/app/api.service';
 import { Reservation } from 'src/app/types/reservation';
 import { UserService } from 'src/app/user/user.service';
 import { PresentService } from '../present.service';
+import { ActivatedRoute } from '@angular/router';
+import { Present } from 'src/app/types/present';
 
 @Component({
   selector: 'app-reserve-present',
@@ -12,20 +14,19 @@ import { PresentService } from '../present.service';
 })
 export class ReservePresentComponent implements OnInit {
   reservations: Reservation[] | null = [];
+  present = {} as Present;
   showReserveMode: boolean = false;
+  id = '';
 
   constructor(private apiService: ApiService,
     private fb: FormBuilder,
     private userService: UserService,
-    private presentService: PresentService
+    private presentService: PresentService,
+    private activeRoute: ActivatedRoute,
   ) { }
 
   get user() {
     return this.userService.user?._id
-  }
-
-  get present() {
-    return this.presentService.present
   }
 
   form = this.fb.group({
@@ -36,14 +37,19 @@ export class ReservePresentComponent implements OnInit {
     this.apiService.getReservations().subscribe({
             next: (reservations) => {
         this.reservations = reservations;
-        console.log("User:", this.user)
-        console.log("Present:", this.present)
-        console.log("Reservations:", this.reservations)
+        console.log("User:", this.user);  
+        console.log("Reservations:", this.reservations);
       },
       error: (err) => {
         console.error('Error: ', err)
       }
-    })    
+    })
+    this.activeRoute.params.subscribe((data) => {
+      this.id = data['presentId'];
+      this.apiService.getPresentById(this.id).subscribe((present) => {
+        this.present = present;             
+      });
+    });   
   }
 
   createReservation(form: NgForm) {
@@ -53,6 +59,6 @@ export class ReservePresentComponent implements OnInit {
 
     const { reservationComment } = form.value;
 
-    this.apiService.createReservation(reservationComment, this.user!, this.present?._id!)
+    this.apiService.createReservation(reservationComment, this.user!, this.present._id!)
   }
 }
